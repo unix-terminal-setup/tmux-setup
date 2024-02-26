c17da8bef0c1c12059af43e0f8328d9c41d00f46
Coenraad Human <coen.human@gmail.com>
2024-02-26 12:20:09 +0200

    feat: includes default configuration from dreams of code

--- setup.sh
+++ setup.sh
@@ -2,4 +2,12 @@
 
 set -euo pipefail
 
-sudo apt install tmux -y
+sudo apt install tmux curl git -y
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+mkdir -p ~/.config/tmux/
+
+curl https://raw.githubusercontent.com/dreamsofcode-io/tmux/main/tmux.conf -o ~/.config/tmux/tmux.conf
+
+tmux source ~/.config/tmux/tmux.conf
